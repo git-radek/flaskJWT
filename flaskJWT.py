ccc4de5a2fa071781897220e483d26998ca620aa
Radosław Michoń <git@blaze.pl>
2021-01-06 10:43:01 +0100

    Create flaskJWT.py

---
+++ flaskJWT.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+from flask import Flask, jsonify, request, render_template
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    jwt_refresh_token_required, create_refresh_token,
+    get_jwt_identity, set_access_cookies,
+    set_refresh_cookies, unset_jwt_cookies
+)
+
+app = Flask(__name__)
+
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+app.config['JWT_COOKIE_SECURE'] = False
+app.config['JWT_ACCESS_COOKIE_PATH'] = '/api/'
+app.config['JWT_REFRESH_COOKIE_PATH'] = '/token/refresh'
+app.config['JWT_COOKIE_CSRF_PROTECT'] = True
+app.config['JWT_SECRET_KEY'] = '<secret>'
+
+jwt = JWTManager(app)
+
+
+@app.route('/')
+def home():
+    return render_template('home.htm')
+@app.route('/token/auth', methods=['POST'])
+def login():
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if username != 'flask-api' or password != '<secret>':
+        return jsonify({'login': False}), 401
+
+    access_token = create_access_token(identity=username)
+    refresh_token = create_refresh_token(identity=username)
+
+    resp = jsonify({'login': True})
+    set_access_cookies(resp, access_token)
+    set_refresh_cookies(resp, refresh_token)
+    return resp, 200
+@app.route('/token/refresh', methods=['POST'])
+@jwt_refresh_token_required
+def refresh():
+    current_user = get_jwt_identity()
+    access_token = create_access_token(identity=current_user)
+
+    resp = jsonify({'refresh': True})
+    set_access_cookies(resp, access_token)
+    return resp, 200
+
+@app.route('/token/logout', methods=['GET'])
+def logout():
+    resp = jsonify({'logout': True})
+    unset_jwt_cookies(resp)
+    return resp, 200
+
+@app.route('/api/testfunction', methods=['GET'])
+@jwt_required
+def protected():
+    username = get_jwt_identity()    
+    return jsonify({'hello': 'from {}'.format(username)}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8001, debug=False)
